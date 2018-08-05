f2d33576f64fe5819b4478cf4e7902fdbf433ef5
SijieShen <sijie.shen@dji.com>
2018-08-06 02:03:01 +0900

    add mac native window support code

---
+++ Platform/Darwin/CocoaApplication.h
@@ -0,0 +1,23 @@
+#include "BaseApplication.hpp"
+#ifdef __OBJC__
+#include <Cocoa/Cocoa.h>
+#endif
+
+namespace newbieGE {
+    class CocoaApplication : public BaseApplication
+    {
+    public:
+        CocoaApplication(GfxConfiguration& config)
+            : BaseApplication(config) {};
+
+        virtual int Initialize();
+        virtual void Finalize();
+        // One cycle of the main loop
+        virtual void Tick();
+
+    protected:
+#ifdef __OBJC__
+        NSWindow* m_pWindow;
+#endif
+    };
+}
