c7769803b0e5c6f6159f7c3f217b9a4f30554966
kiorisyshen <kiorisyshen@gmail.com>
2019-05-11 20:59:56 +0900

    Update crossguid build rule

--- External/build_crossguid.sh
+++ External/build_crossguid.sh
@@ -2,7 +2,7 @@
 mkdir -p build/crossguid
 cd build/crossguid
 cmake -DCMAKE_INSTALL_PREFIX=../../ ../../src/crossguid
-cmake --build . --config Release
+cmake --build . --config Release --target install
 
 # install
 cd -
