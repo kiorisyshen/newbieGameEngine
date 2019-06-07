9d1e07cb55ba6602f0d66fbb4fbc1ea1d27c5e5b
kiorisyshen <kiorisyshen@gmail.com>
2019-06-07 13:50:29 +0900

    Modify test building problem, add bullet test code

--- Platform/Empty/EmptyApplication.cpp
+++ Platform/Empty/EmptyApplication.cpp
@@ -7,5 +7,7 @@ namespace newbieGE {
     MemoryManager*   g_pMemoryManager   = static_cast<MemoryManager*>(new MemoryManager);
     AssetLoader*     g_pAssetLoader     = static_cast<AssetLoader*>(new AssetLoader);
     SceneManager*    g_pSceneManager    = static_cast<SceneManager*>(new SceneManager);
+    InputManager*    g_pInputManager    = static_cast<InputManager*>(new InputManager);
+    PhysicsManager*  g_pPhysicsManager  = static_cast<PhysicsManager*>(new PhysicsManager);
 }
 
