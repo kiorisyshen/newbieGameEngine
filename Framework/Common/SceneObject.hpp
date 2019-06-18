934fe2058dc3f550d15ca7111d429fe8c3537f0b
kiorisyshen <kiorisyshen@gmail.com>
2019-06-18 19:02:04 +0900

    Fix 24 bit texture in metal rendering

--- Framework/Common/SceneObject.hpp
+++ Framework/Common/SceneObject.hpp
@@ -344,8 +344,65 @@ namespace newbieGE {
                     LoadTexture();
                 }
 
+                AdjustTextureBitcount();
+
                 return *m_pImage; 
             };
+
+            void AdjustTextureBitcount()
+            {
+                // GPU does not support 24bit and 48bit textures, so adjust it
+                if (m_pImage->bitcount == 24)
+                {
+                    // DXGI does not have 24bit formats so we have to extend it to 32bit
+                    uint32_t new_pitch = m_pImage->pitch / 3 * 4;
+                    size_t data_size = new_pitch * m_pImage->Height;
+                    uint8_t* data = new uint8_t[data_size];
+                    uint8_t* buf;
+                    uint8_t* src;
+                    for (uint32_t row = 0; row < m_pImage->Height; row++) {
+                        buf = data + row * new_pitch;
+                        src = static_cast<uint8_t*>(m_pImage->data) + row * m_pImage->pitch;
+                        for (uint32_t col = 0; col < m_pImage->Width; col++) {
+                            memcpy(buf, src, 3);
+                            memset(buf+3, 0x00, 1);  // set alpha to 0
+                            buf += 4;
+                            src += 3;
+                        }
+                    }
+
+                    delete static_cast<uint8_t*>(m_pImage->data);
+                    m_pImage->data = data;
+                    m_pImage->data_size = data_size;
+                    m_pImage->pitch = new_pitch;
+                    m_pImage->bitcount = 32;
+                }
+                else if (m_pImage->bitcount == 48)
+                {
+                    // DXGI does not have 48bit formats so we have to extend it to 64bit
+                    uint32_t new_pitch = m_pImage->pitch / 3 * 4;
+                    size_t data_size = new_pitch * m_pImage->Height;
+                    uint8_t* data = new uint8_t[data_size];
+                    uint8_t* buf;
+                    uint8_t* src;
+                    for (uint32_t row = 0; row < m_pImage->Height; row++) {
+                        buf = data + row * new_pitch;
+                        src = static_cast<uint8_t*>(m_pImage->data) + row * m_pImage->pitch;
+                        for (uint32_t col = 0; col < m_pImage->Width; col++) {
+                            memcpy(buf, src, 6);
+                            memset(buf+6, 0x00, 2); // set alpha to 0
+                            buf += 8;
+                            src += 6;
+                        }
+                    }
+
+                    delete static_cast<uint8_t*>(m_pImage->data);
+                    m_pImage->data = data;
+                    m_pImage->data_size = data_size;
+                    m_pImage->pitch = new_pitch;
+                    m_pImage->bitcount = 64;
+                }
+            }
 
         friend std::ostream& operator<<(std::ostream& out, const SceneObjectTexture& obj);
     };
