684a8ba3a358302d5fc1497e24caedbe93975126
kiorisyshen <kiorisyshen@gmail.com>
2019-07-19 10:50:19 +0900

    Format cmake, add editor

---
+++ Editor/EditorLogic.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "IGameLogic.hpp"
+
+namespace newbieGE
+{
+class EditorLogic : implements IGameLogic
+{
+    int Initialize();
+    void Finalize();
+    void Tick();
+
+    void OnLeftKeyDown();
+    void OnRightKeyDown();
+    void OnUpKeyDown();
+    void OnDownKeyDown();
+
+    void OnAnalogStick(int id, float deltaX, float deltaY);
+#ifdef DEBUG
+    void DrawDebugInfo();
+#endif
+};
+} // namespace newbieGE
