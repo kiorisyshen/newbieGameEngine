44cb362f7ffb6d24097091fca5f439c683dc24aa
kiorisyshen <kiorisyshen@gmail.com>
2019-10-30 01:10:08 +0900

    Build external using emscripten

---
+++ External/build_bullet-emscripten.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+# git submodule update --init External/src/bullet
+mkdir -p build/bullet
+cd build/bullet
+cmake -DCMAKE_TOOLCHAIN_FILE=../../../cmake/Emscripten.cmake -DCMAKE_INSTALL_PREFIX=../../Emscripten/ -DCMAKE_INSTALL_RPATH=../../Emscripten -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=RELEASE ../../src/bullet3 || exit 1
+cmake --build . --config release --target install
+echo "Completed build of Bullet."
+
--- build_webassembly.sh
+++ build_webassembly.sh
@@ -2,10 +2,8 @@
 set -e
 # source /Users/shen/Learning/emsdk/emsdk_env.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/External/$(uname -s)/lib
-export EMSCRIPTEN=/Users/shen/Learning/emsdk/upstream/emscripten
 mkdir -p build/WA/Debug
 cd build/WA
-rm -rf Debug
 mkdir -p Debug
 cd Debug
 cmake -DCMAKE_TOOLCHAIN_FILE=../../../cmake/Emscripten.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../.. 
