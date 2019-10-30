2b3be5e6daa9f8135428eda2f38976232422de22
kiorisyshen <kiorisyshen@gmail.com>
2019-10-30 12:16:11 +0900

    Update build script

--- External/build_crossguid.sh
+++ External/build_crossguid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p build/crossguid
+rm -rf build/crossguid/*
 cd build/crossguid
 cmake -DCMAKE_INSTALL_PREFIX=../../ ../../src/crossguid
 cmake --build . --config Release --target install
